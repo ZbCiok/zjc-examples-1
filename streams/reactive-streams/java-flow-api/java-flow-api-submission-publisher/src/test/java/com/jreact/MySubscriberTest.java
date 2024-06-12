7c6fbe2afecbaad4de00b258f28a5f8f80906853
zbyszek <poltman@onet.pl>
2024-06-12 17:32:48 +0200

    flow SubmissionPublisher 01

---
+++ streams/reactive-streams/java-flow-api/java-flow-api-submission-publisher/src/test/java/com/jreact/MySubscriberTest.java
@@ -0,0 +1,13 @@
+package com.jreact;
+
+import org.junit.Test;
+
+public class MySubscriberTest {
+
+    MySubscriber mySubscriber = new MySubscriber();
+
+    @Test
+    public void flowPublisher() {
+        mySubscriber.flowSubmissionPublisher();
+    }
+}
